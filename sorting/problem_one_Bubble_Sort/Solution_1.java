a763ec8edef7c4c38e575ca280bc7e8143deceb6
therealkundanpatil <kp28021996@gmail.com>
2021-02-24 12:13:12 -0500

    -Implemented Bubble Sort Algorithm

--- sorting/problem_one_Bubble_Sort/Solution_1.java
+++ sorting/problem_one_Bubble_Sort/Solution_1.java
@@ -1,5 +1,36 @@
 package sorting.problem_one_Bubble_Sort;
 
-public class Solution_1 {
+import java.util.Arrays;
 
+/**
+ * Bubble Sort Algorithm
+ * 
+ * Time Complexity : O(n^2)
+ * 
+ * Space Complexity : O(1)
+ * 
+ * @author Kundan
+ *
+ */
+public class Solution_1 {
+	public static int[] bubbleSort(int[] array) {
+		// Write your code here.
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+
+			}
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {5,4,3,2,1};
+		int[] b = bubbleSort(a);
+		System.out.println(Arrays.toString(b));
+	}
 }
