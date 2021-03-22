d2ddf0728bfcf853fb40820dc724b4d329f9cd3e
therealkundanpatil <kp28021996@gmail.com>
2021-03-22 15:16:24 -0400

    -Implemented and Tested Recursive Binary Search Algirithm Technique

--- searching/problem_one_Binary_Search/Solution_1.java
+++ searching/problem_one_Binary_Search/Solution_1.java
@@ -12,34 +12,31 @@ package searching.problem_one_Binary_Search;
  */
 public class Solution_1 {
 	public static int binarySearch(int[] array, int target) {
-		return binarySearch(array,target,0,array.length-1);
+		return binarySearch(array, target, 0, array.length - 1);
 	}
-	
+
 	private static int binarySearch(int[] array, int target, int left, int right) {
-		while(left <= right) {
-			int middle = left + (right-left) / 2;
+		while (left <= right) {
+			int middle = left + (right - left) / 2;
 			int data = array[middle];
-			
-			if(data == target) {
+
+			if (data == target) {
 				return middle;
-			}
-			else if(data < target) {
+			} else if (data < target) {
 				left = middle + 1;
-			}
-			else {
+			} else {
 				right = middle - 1;
 			}
 		}
 		return -1;
 	}
-	
+
 	public static void main(String args[]) {
-		int[] a = {1,2,3,4,5,6,7,8,9,10};
+		int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		int index = binarySearch(a, 5);
-		if(index != -1) {
-			System.out.println("Element found at index : "+index);
-		}
-		else {
+		if (index != -1) {
+			System.out.println("Element found at index : " + index);
+		} else {
 			System.out.println("Element not found");
 		}
 	}
--- searching/problem_one_Binary_Search/Solution_2.java
+++ searching/problem_one_Binary_Search/Solution_2.java
@@ -1,5 +1,47 @@
 package searching.problem_one_Binary_Search;
 
+/**
+ * Binary Search Algorithm
+ * 
+ * Time Complexity : O(Log(N))
+ * 
+ * Space Complexity : O(1)
+ * 
+ * @author Kundan
+ *
+ */
 public class Solution_2 {
+	public static int binarySearch(int[] array, int target) {
+		// Write your code here.
+		int left = 0;
+		int right = array.length - 1;
+		return binarySearch(array, target, left, right);
+	}
 
+	private static int binarySearch(int[] array, int target, int left, int right) {
+		if (left > right) {
+			return -1;
+		}
+
+		int middle = left + (right - left) / 2;
+		int data = array[middle];
+
+		if (data == target) {
+			return middle;
+		} else if (data < target) {
+			return binarySearch(array, target, middle + 1, right);
+		} else {
+			return binarySearch(array, target, left, middle - 1);
+		}
+	}
+
+	public static void main(String args[]) {
+		int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int index = binarySearch(a, 5);
+		if (index != -1) {
+			System.out.println("Element found at index : " + index);
+		} else {
+			System.out.println("Element not found");
+		}
+	}
 }
