c828f98b6eefcc2bd0a07f8bcd6fc9cab3f0051a
therealkundanpatil <kp28021996@gmail.com>
2021-03-22 14:18:01 -0400

    -Implemented Binary sort algorithm

--- searching/problem_one_Binary_Search/Solution_1.java
+++ searching/problem_one_Binary_Search/Solution_1.java
@@ -1,5 +1,21 @@
 package searching.problem_one_Binary_Search;
 
 public class Solution_1 {
+	public static int binarySearch(int[] array, int target) {
+		// Write your code here.
+		int left = 0;
+		int right = array.length - 1;
 
+		while (left <= right) {
+			int middle = left + (right - left) / 2;
+			if (array[middle] == target) {
+				return middle;
+			} else if (array[middle] < target) {
+				left = middle + 1;
+			} else {
+				right = middle - 1;
+			}
+		}
+		return -1;
+	}
 }
