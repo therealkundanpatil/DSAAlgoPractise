e0ebeeaa6a96681af31f969051984a4db6f4c595
therealkundanpatil <kp28021996@gmail.com>
2021-03-22 14:27:28 -0400

    -Implemented Iterative version of Binary Search Algorithm

--- searching/problem_one_Binary_Search/Solution_1.java
+++ searching/problem_one_Binary_Search/Solution_1.java
@@ -1,21 +1,46 @@
 package searching.problem_one_Binary_Search;
 
+/**
+ * Binary Search Algorithm
+ * 
+ * Time Complexity : O(log(N))
+ * 
+ * Space Complexity : O(1)
+ * 
+ * @author Kundan
+ *
+ */
 public class Solution_1 {
 	public static int binarySearch(int[] array, int target) {
-		// Write your code here.
-		int left = 0;
-		int right = array.length - 1;
-
-		while (left <= right) {
-			int middle = left + (right - left) / 2;
-			if (array[middle] == target) {
+		return binarySearch(array,target,0,array.length-1);
+	}
+	
+	private static int binarySearch(int[] array, int target, int left, int right) {
+		while(left <= right) {
+			int middle = left + (right-left) / 2;
+			int data = array[middle];
+			
+			if(data == target) {
 				return middle;
-			} else if (array[middle] < target) {
+			}
+			else if(data < target) {
 				left = middle + 1;
-			} else {
+			}
+			else {
 				right = middle - 1;
 			}
 		}
 		return -1;
+	}
+	
+	public static void main(String args[]) {
+		int[] a = {1,2,3,4,5,6,7,8,9,10};
+		int index = binarySearch(a, 5);
+		if(index != -1) {
+			System.out.println("Element found at index : "+index);
+		}
+		else {
+			System.out.println("Element not found");
+		}
 	}
 }
