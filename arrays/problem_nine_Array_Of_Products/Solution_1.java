aa3085c3b2299fbad6af3e8f1537eeaab5f42fba
therealkundanpatil <kp28021996@gmail.com>
2021-06-04 23:33:22 -0400

    Implemented in O(n^2) time / O(n) space

--- arrays/problem_nine_Array_Of_Products/Solution_1.java
+++ arrays/problem_nine_Array_Of_Products/Solution_1.java
@@ -1,5 +1,27 @@
 package arrays.problem_nine_Array_Of_Products;
 
-public class Solution_1 {
+import java.util.Arrays;
 
+public class Solution_1 {
+	public static int[] arrayOfProducts(int[] array) {
+		// Write your code here.
+		int[] result = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			int answer = 1;
+			for (int j = 0; j < array.length; j++) {
+				if (i == j) {
+					continue;
+				} else {
+					answer *= array[j];
+				}
+			}
+			result[i] = answer;
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {5,1,4,2};
+		System.out.println(Arrays.toString(arrayOfProducts(array)));
+	}
 }
