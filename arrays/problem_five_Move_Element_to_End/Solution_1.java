7c1fecd5984b6251b8e057da795f8c83297b8647
therealkundanpatil <kp28021996@gmail.com>
2021-06-04 23:26:49 -0400

    Implemented in O(n) time / O(n) space

--- arrays/problem_five_Move_Element_to_End/Solution_1.java
+++ arrays/problem_five_Move_Element_to_End/Solution_1.java
@@ -1,5 +1,57 @@
 package arrays.problem_five_Move_Element_to_End;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution_1 {
+	public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+		// Write your code here.
+		int fixedLength = array.size();
+
+		int[] data = new int[fixedLength];
+		for (int i = 0; i < array.size(); i++) {
+			data[i] = array.get(i);
+		}
+
+		for (int i = 0, j = fixedLength - 1; i < j;) {
+			if (data[i] != toMove && data[j] != toMove) {
+				i++;
+			} else if (data[j] == toMove) {
+				j--;
+			} else if (data[i] == toMove && data[j] != toMove) {
+				int temp = data[i];
+				data[i] = data[j];
+				data[j] = temp;
+
+				i++;
+				j--;
+			}
+
+		}
+
+		List<Integer> toReturn = new ArrayList<>();
+
+		for (int i = 0; i < fixedLength; i++) {
+			toReturn.add(data[i]);
+		}
+
+		return toReturn;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> array = new ArrayList<>();
+		array.add(2);
+		array.add(1);
+		array.add(2);
+		array.add(2);
+		array.add(2);
+		array.add(3);
+		array.add(4);
+		array.add(2);
+
+		int toMove = 2;
+
+		System.out.println(moveElementToEnd(array, toMove));
+	}
 
 }
