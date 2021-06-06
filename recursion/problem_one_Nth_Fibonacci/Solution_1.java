9938ace6146107c3ca2e3f42579aff0f606fffba
therealkundanpatil <kp28021996@gmail.com>
2021-06-05 20:42:38 -0400

    Implemented in O(2^n) time / O(1) space

--- recursion/problem_one_Nth_Fibonacci/Solution_1.java
+++ recursion/problem_one_Nth_Fibonacci/Solution_1.java
@@ -1,5 +1,18 @@
 package recursion.problem_one_Nth_Fibonacci;
 
 public class Solution_1 {
-
+	public static int getNthFib(int n) {
+		// Write your code here.
+		if (n == 2) {
+			return 1;
+		} else if (n == 1) {
+			return 0;
+		} else {
+			return getNthFib(n - 1) + getNthFib(n - 2);
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getNthFib(6));
+	}
 }
