349725bc0329935886b5568def67677fd6c3c478
therealkundanpatil <kp28021996@gmail.com>
2021-06-12 16:27:01 -0400

    Implemented in O(n) time / O(1) space

--- famous_algorithms/problem_one_Kadane_Algorithm/Solution_2.java
+++ famous_algorithms/problem_one_Kadane_Algorithm/Solution_2.java
@@ -1,5 +1,21 @@
 package famous_algorithms.problem_one_Kadane_Algorithm;
 
 public class Solution_2 {
+	public static int kadanesAlgorithm(int[] array) {
+		// Write your code here.
+		int maximumSum = array[0];
+		int currentSum = array[0];
 
+		for (int i = 1; i < array.length; i++) {
+			currentSum = Math.max(currentSum + array[i], array[i]);
+			maximumSum = Math.max(currentSum, maximumSum);
+		}
+
+		return maximumSum;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println(kadanesAlgorithm(array));
+	}
 }
