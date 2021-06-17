7fb0df420546fcfec2c51783a036b868805843e3
therealkundanpatil <kp28021996@gmail.com>
2021-06-17 00:35:58 -0400

    Implemented in O(n) time / O(n) space Hashet version

--- arrays/problem_ten_First_Duplicate_Value/Solution_2.java
+++ arrays/problem_ten_First_Duplicate_Value/Solution_2.java
@@ -1,5 +1,23 @@
 package arrays.problem_ten_First_Duplicate_Value;
 
-public class Solution_2 {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Solution_2 {
+	public static int firstDuplicateValue(int[] array) {
+		// Write your code here.
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < array.length; i++) {
+			if (set.contains(array[i])) {
+				return array[i];
+			}
+			set.add(array[i]);
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 2, 1, 5, 2, 3, 3, 4 };
+		System.out.println(firstDuplicateValue(array));
+	}
 }
