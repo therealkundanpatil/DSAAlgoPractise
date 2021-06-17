cd1dda9da896f83f009fa8fc0a8db9ead17cca66
therealkundanpatil <kp28021996@gmail.com>
2021-06-17 00:36:38 -0400

    Implemented in O(n) time / O(1) space array version

--- arrays/problem_ten_First_Duplicate_Value/Solution_3.java
+++ arrays/problem_ten_First_Duplicate_Value/Solution_3.java
@@ -1,5 +1,20 @@
 package arrays.problem_ten_First_Duplicate_Value;
 
 public class Solution_3 {
-
+	public static int firstDuplicateValue(int[] array) {
+		// Write your code here.
+		for (int i = 0; i < array.length; i++) {
+			int value = Math.abs(array[i]);
+			if (array[value - 1] < 0) {
+				return value;
+			}
+			array[value - 1] *= -1;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = { 2, 1, 5, 2, 3, 3, 4 };
+		System.out.println(firstDuplicateValue(array));
+	}
 }
